570cb91cc7934e6ff8d1b5c91429a6dfeb8e9f5d
Rupesh Yogi <rupeshyogi712@gmail.com>
2026-01-29 06:49:54 +0000

    finished js code

--- calculator.js
+++ calculator.js
@@ -3,60 +3,61 @@ const buttons = document.querySelectorAll("button");
 const input = document.querySelector(".display");
 
 
-// buttons.addEventListener("click", ()=> {
-// input.value += btn.textContent;
-// })
-
-// buttons.forEach(btn => {
-//   btn.addEventListener("click", () => {
-//     input.value += btn.textContent;
-//   });
-// });
-
-
-// buttons.forEach(btn => {
-//   btn.addEventListener("click", () => {
-//     const value = btn.textContent;
-
-//     if (value === "CE") {
-//       input.value = ""; // Clear display
-//     } 
-//     else if (value === "=") {
-//       input.value = eval(input.value); // Calculate
-//     } 
-//     else {
-//       input.value += value; // Add number/operator
-//     }
-//   });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 buttons.forEach(btn => {
-  btn.addEventListener("click", ()=> {
-    input.value = input.value + btn.textContent;
-  })
-})
+  btn.addEventListener("click", () => {
+    // input.value = input.value + btn.textContent;  ---> this will make to 
+    // act first rather than thinking which button 
+    // without that the button will be observed first and then only act
+
+
+    const value = btn.textContent;
+
+
+    if (value == "CE") {
+      input.value = "";
+    } 
+    else if (value == "=") {
+      input.value = eval(input.value);
+    } 
+    else {
+      input.value += value;
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
